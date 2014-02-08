bfaf4c4e7cefa44919c45c7506b09cd59f031d7e
Bruno da Luz <brunodaluz@users.noreply.github.com>
2014-02-08 01:48:29 -0200

    Update simple_curl.c

--- simple_curl.c
+++ simple_curl.c
@@ -18,7 +18,7 @@ int main() {
 
  curl = curl_easy_init();
  if(curl) {
-  curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
+  curl_easy_setopt(curl, CURLOPT_URL, "http://www.example.com");
   curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION,1L);
   res = curl_easy_perform(curl);
   if(res != CURLE_OK)
